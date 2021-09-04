91b2166d6efe041ceaba36d2b903fdd5f25c2d81
Jessica <jessica.andreoli@yahoo.com.br>
2021-09-04 15:50:04 -0300

    Criando model de Client

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const database = require('./config/database-connect')
+const Client = require('./src/model/client')
 require('dotenv').config()
 
 database.connect()
