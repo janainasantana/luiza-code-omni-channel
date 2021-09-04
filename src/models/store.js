472bb0c424a8e6ad3eafc6d0974161b09386d6d2
Jennifer Dominique <j.dominiqueds@gmail.com>
2021-09-04 17:42:14 -0300

    feature/22 criando a tabela de store

---
+++ src/models/store.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const Store = sequelize.define('Store', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING(80),
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: new Date()
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: new Date()
+    }
+  })
+
+  return Store
+}
