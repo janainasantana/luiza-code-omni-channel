95f82abf24ad86c33553c6ff3d35ac6fb2ac4384
Janaina <72051834+janainasantana@users.noreply.github.com>
2021-09-06 22:17:16 -0300

    feature/13 Criando API para atualizar Status do pedido Retirado (#17)
    * feature/13 Criando API para atualizar Status do pedido Retirado
    
    * feature/13 Alterando lógica da atualização do status do pedido

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
-const ProductRoute = require("./product-route")
-const ClientRoute = require("./client-route")
-const StoreRoute = require("./store-route")
+const ProductRoute = require ("./product-route")
+const ClientRoute = require ("./client-route")
+const StoreRoute = require ("./store-route")
+const RetrieveOrderRoute = require ("./retrieve-order-route")
 const OrderRoute = require("./order-route")
 
 module.exports = app => {
     ProductRoute(app),
     ClientRoute(app),
     StoreRoute(app),
+    RetrieveOrderRoute(app),
     OrderRoute(app)
 }
 
