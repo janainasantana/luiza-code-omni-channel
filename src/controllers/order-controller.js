f6d751eff2b36e0d9abf59f6e4bbcdf78e6512c7
Jennifer Dominique <j.dominiqueds@gmail.com>
2021-09-07 19:50:37 -0300

    feature/15 encontrando os pedidos de um cliente (#23)
    * feature/15 encontrando os pedidos de um cliente
    
    * feature/15 adicionando a rota no index das rotas
    
    * feature/15 alterando o id_cliente  para inteiro
    
    * feature/15 alterando todos os comandos para um único arquivo referenciado
    
    * feature/15 alterando a rota no index

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -1,5 +1,6 @@
 const { Order } = require("./../models")
 
+//CREATE ORDER
 exports.post = async (req, res) => {
     const data = req.body
 
@@ -13,6 +14,24 @@ exports.post = async (req, res) => {
     } catch (error) {
         return response(res, 400, `Cannot save order, error: ${error}`)
     }
+}
+
+//FIND CLIENT ORDER
+exports.getOrdersByClient = async (req, res) => {
+    const clientId = parseInt(req.params.clientId)
+    
+    try{
+        const orders = await Order.findAll({ where: {id_client: clientId}}, 
+            {
+            limit: parseInt(req.query.limit) || 10, 
+            offset: parseInt(req.query.offset) || 0
+            }
+        )
+        return response(res, 200, orders)
+    }
+    catch(error){
+        return response(res, 404, `We can't find client orders, error: ${error}`)
+    }  
 }
 
 response = (res, status, data) => {
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,11 +1,11 @@
 const { Product } = require("./../models")
-exports.get = async (req, res) => {
 
+exports.get = async (req, res) => {
     const products = await Product.findAll(
         {
-            limit: parseInt(req.query.limit) || 10, offset: parseInt(req.query.offset) || 0
+            limit: parseInt(req.query.limit) || 10, 
+            offset: parseInt(req.query.offset) || 0
         }
     )
     res.send(products)
-
 }
