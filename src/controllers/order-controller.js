56cb855e49036f4a1a41c6fd0cd3cfadd4933f8a
Janaina do Carmo Santana <janacsantana@gmail.com>
2021-09-07 20:00:29 -0300

    feature/13 Ajustando apontamentos de revisão de código.

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -15,6 +15,38 @@ exports.post = async (req, res) => {
     }
 }
 
+exports.patchRetrieve = async (req, res) => {
+    const orderId = req.params.id
+    const order = await Order.findByPk(orderId)
+    if (!order) {
+        return response(res, 404, "Order not found.")
+    } else {
+        if (order.id_status === 2) {
+            order.id_status = 3
+            await order.save()
+            return response(res, 200, order)
+        } else {
+            return response(res, 422, "Order status cannot be updated.")
+        }
+    }
+}
+
+exports.patchFinish = async (req, res) => {
+    const orderId = req.params.id
+    const order = await Order.findByPk(orderId)
+    if (order === null) {
+        return response(res, 404, "Order not found.")
+    } else {
+        if (order.id_status === 1) {
+            order.id_status = 2
+            await order.save()
+            return response(res, 200, order)
+        } else {
+            return response(res, 422, "Order status cannot be updated.")
+        }
+    }
+}
+
 response = (res, status, data) => {
     res.status(status).json(data)
 }
