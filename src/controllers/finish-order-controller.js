de5e28a996e1767a8ead86c7f678ad498da9f098
Janaina do Carmo Santana <janacsantana@gmail.com>
2021-09-07 10:52:38 -0300

    feature/12 Adicionando regra de atualização de status

--- src/controllers/finish-order-controller.js
+++ src/controllers/finish-order-controller.js
@@ -6,13 +6,16 @@ exports.patch = async (req, res) => {
     if (order === null) {
         return response(res, 404, "Order not found.")
     } else {
-        order.id_status = 2
-        order.updatedAt = new Date()
-        await order.save()
+        if (order.id_status === 1) {
+            order.id_status = 2
+            order.updatedAt = new Date()
+            await order.save()
+            return response(res, 200, order)
+        } else {
+            return response(res, 422, "Order status cannot be updated.")
+        }
     }
-    return response(res, 200, order)
 }
-
 response = (res, status, data) => {
     res.status(status).json(data)
 }
