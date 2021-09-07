0c6f7e49781169e2eae685db03f1be1a2ce1870f
Ana Carolliny <80598323+anacarolliny@users.noreply.github.com>
2021-09-07 20:47:23 -0300

    Feature#11 (#24)
    * adicionando route e controller product
    
    * feature#11 criando API inserir produto em pedido
    
    * feature/11 adicionando produtos no pedido
    
    * feature#11 alterando order product controller para order controller
    
    * alterando order
    
    * feature#11 alterando client id para id
    
    Co-authored-by: Janaina do Carmo Santana <janacsantana@gmail.com>
    Co-authored-by: Jessica <jessica.andreoli@yahoo.com.br>

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -1,70 +1,109 @@
-const { Order } = require("./../models")
+const { OrdersProducts, Order, Product } = require('./../models')
 
 //CREATE ORDER
 exports.post = async (req, res) => {
-    const data = req.body
+  const data = req.body
 
-    try {
-        const order = await Order.create({
-            id_status: 1,
-            id_client: data.id_client,
-            id_store: data.id_store
-        })
-        return response(res, 201, order)
-    } catch (error) {
-        return response(res, 400, `Cannot save order, error: ${error}`)
-    }
+  try {
+    const order = await Order.create({
+      id_status: 1,
+      id_client: data.id_client,
+      id_store: data.id_store
+    })
+    return response(res, 201, order)
+  } catch (error) {
+    return response(res, 400, `Cannot save order, error: ${error}`)
+  }
 }
 
 exports.patchRetrieve = async (req, res) => {
-    const orderId = req.params.id
-    const order = await Order.findByPk(orderId)
-    if (!order) {
-        return response(res, 404, "Order not found.")
+  const orderId = req.params.id
+  const order = await Order.findByPk(orderId)
+  if (!order) {
+    return response(res, 404, 'Order not found.')
+  } else {
+    if (order.id_status === 2) {
+      order.id_status = 3
+      await order.save()
+      return response(res, 200, order)
     } else {
-        if (order.id_status === 2) {
-            order.id_status = 3
-            await order.save()
-            return response(res, 200, order)
-        } else {
-            return response(res, 422, "Order status cannot be updated.")
-        }
+      return response(res, 422, 'Order status cannot be updated.')
     }
+  }
 }
 
 exports.patchFinish = async (req, res) => {
-    const orderId = req.params.id
-    const order = await Order.findByPk(orderId)
-    if (order === null) {
-        return response(res, 404, "Order not found.")
+  const orderId = req.params.id
+  const order = await Order.findByPk(orderId)
+  if (!order) {
+    return response(res, 404, 'Order not found.')
+  } else {
+    if (order.id_status === 1) {
+      order.id_status = 2
+      await order.save()
+      return response(res, 200, order)
     } else {
-        if (order.id_status === 1) {
-            order.id_status = 2
-            await order.save()
-            return response(res, 200, order)
-        } else {
-            return response(res, 422, "Order status cannot be updated.")
-        }
+      return response(res, 422, 'Order status cannot be updated.')
     }
+  }
 }
 
 exports.getOrdersByClient = async (req, res) => {
-    const clientId = parseInt(req.params.clientId)
+  const clientId = parseInt(req.params.id)
 
-    try {
-        const orders = await Order.findAll({ where: { id_client: clientId } },
-            {
-                limit: parseInt(req.query.limit) || 10,
-                offset: parseInt(req.query.offset) || 0
-            }
-        )
-        return response(res, 200, orders)
+  try {
+    const orders = await Order.findAll(
+      { where: { id_client: clientId } },
+      {
+        limit: parseInt(req.query.limit) || 10,
+        offset: parseInt(req.query.offset) || 0
+      }
+    )
+    return response(res, 200, orders)
+  } catch (error) {
+    return response(res, 404, `We can't find client orders, error: ${error}`)
+  }
+}
+
+exports.addProduct = async (req, res) => {
+  const productId = parseInt(req.body.productId)
+  const orderId = parseInt(req.params.id)
+
+  try {
+    const order = await Order.findByPk(orderId)
+    if (!order) {
+      return response(res, 404, 'Cannot add product. Order not found.')
     }
-    catch (error) {
-        return response(res, 404, `We can't find client orders, error: ${error}`)
+    if (order.id_status !== 1) {
+      return response(
+        res,
+        422,
+        'Cannot add product when Order status is not created.'
+      )
     }
+    const product = await Product.findByPk(productId)
+    if (!product) {
+      return response(res, 404, 'Product not found.')
+    }
+    let orderProduct = await OrdersProducts.findOne({
+      where: { id_order: orderId, id_product: productId }
+    })
+
+    if (!orderProduct) {
+      orderProduct = await OrdersProducts.create({
+        id_order: orderId,
+        id_product: productId
+      })
+    } else {
+      return response(res, 422, 'Product already exists in this order.')
+    }
+
+    return response(res, 201, orderProduct)
+  } catch (error) {
+    return response(res, 400, `Cannot save product, error: ${error}`)
+  }
 }
 
 response = (res, status, data) => {
-    res.status(status).json(data)
+  res.status(status).json(data)
 }
--- src/routes/order-route.js
+++ src/routes/order-route.js
@@ -1,8 +1,9 @@
-const OrderController = require("./../controllers/order-controller")
+const OrderController = require('./../controllers/order-controller')
 
-module.exports = (app) => {
-    app.post("/orders", OrderController.post)
-    app.patch("/orders/:id/finish", OrderController.patchFinish)
-    app.patch("/orders/:id/retrieve", OrderController.patchRetrieve)
-    app.getOrdersByClient("/orders/client/:id", OrderController.getOrdersByClient)
+module.exports = app => {
+  app.post('/orders', OrderController.post)
+  app.patch('/orders/:id/finish', OrderController.patchFinish)
+  app.patch('/orders/:id/retrieve', OrderController.patchRetrieve)
+  app.get('/orders/client/:id', OrderController.getOrdersByClient)
+  app.post('/orders/:id/products', OrderController.addProduct)
 }
