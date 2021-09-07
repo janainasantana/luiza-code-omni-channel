e869abbb082077f3633a679152a69259fd170135
Jessica <jessica.andreoli@yahoo.com.br>
2021-09-07 20:36:23 -0300

    feature#11 alterando order product controller para order controller

--- src/routes/orderProduct-route.js
+++
@@ -1,5 +0,0 @@
-const OrderProductController = require("./../controllers/orderProduct-controller")
-
-module.exports = (app) => {
-    app.post("/orders/:id/products", OrderProductController.post)
-}
--- src/routes/products-orders.js
+++
@@ -1,6 +0,0 @@
-const ProductsOrdersController = require("./../controllers/products-orders-controller")
-
-module.exports = (app) => {
-    app.post("/products-orders", ProductsOrdersController.post)
-}
-
