bdc49fb4dcd184b95a5ba1d67edce72dde4f2ca2
Janaina <72051834+janainasantana@users.noreply.github.com>
2021-09-06 21:35:57 -0300

    Feature/9 (#15)
    * [wip] feature/9 Criando api de criar pedido
    
    * feature/9 Finalizando api de criar pedido

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
-const ProductRoute = require ("./product-route")
-const ClientRoute = require ("./client-route")
-const StoreRoute = require ("./store-route")
+const ProductRoute = require("./product-route")
+const ClientRoute = require("./client-route")
+const StoreRoute = require("./store-route")
+const OrderRoute = require("./order-route")
+
 module.exports = app => {
     ProductRoute(app),
     ClientRoute(app),
-    StoreRoute(app)
-    
+    StoreRoute(app),
+    OrderRoute(app)
 }
 
