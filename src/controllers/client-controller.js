481b6326307d42e7a6724f36a70c1472bcc3df91
Jessica <jessica.andreoli@yahoo.com.br>
2021-09-07 18:04:18 -0300

    incluindo try cath

--- src/controllers/client-controller.js
+++ src/controllers/client-controller.js
@@ -8,27 +8,31 @@ const Op = Sequelize.Op
 exports.post = async (req, res) => {
   let data = req.body
 
-  data.cpf = data.cpf.replace('.', '').replace('.', '').replace('-', '')
+  try {
+    data.cpf = data.cpf.replace('.', '').replace('.', '').replace('-', '')
 
-  if (!cpf.isValid(data.cpf)) return response(res, 400, `CPF format is invalid`)
+    if (!cpf.isValid(data.cpf))
+      return response(res, 400, `CPF format is invalid`)
 
-  if (!validator.validate(data.email))
-    return response(res, 400, `Email format is invalid`)
+    if (!validator.validate(data.email))
+      return response(res, 400, `Email format is invalid`)
 
-  const dbClient = await Client.findOne({ where: { cpf: data.cpf } })
-  if (dbClient) return response(res, 409, `CPF to be unique`)
+    const dbClient = await Client.findOne({ where: { cpf: data.cpf } })
+    if (dbClient) return response(res, 409, `CPF to be unique`)
 
-  const dbClientEmail = await Client.findOne({ where: { email: data.email } })
-  if (dbClientEmail) return response(res, 409, `email to be unique`)
+    const dbClientEmail = await Client.findOne({ where: { email: data.email } })
+    if (dbClient) return response(res, 409, `email to be unique`)
 
-  const client = await Client.create({
-    name: data.name,
-    cpf: data.cpf,
-    email: data.email
-  })
+    const client = await Client.create({
+      name: data.name,
+      cpf: data.cpf,
+      email: data.email
+    })
 
-  if (client) {
-    return response(res, 201, client)
+    if (client) {
+      return response(res, 201, client)
+    }
+  } catch {
+    return response(res, 400, `Cannot save client, error: ${error}`)
   }
-  return response(res, 400, `Cannot save client, error: ${error}`)
 }
