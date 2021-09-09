4db6811ae08a165d7292c90c09ec3f0ddeaf237a
Jessica <jessica.andreoli@yahoo.com.br>
2021-09-08 22:33:22 -0300

    feature/32 iniciando swagger

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 require('./src/routes/index')(app)
 require('dotenv').config()
--- src/controllers/store-controller.js
+++ src/controllers/store-controller.js
@@ -1,8 +1,26 @@
 const { Store } = require('./../models')
 exports.get = async (req, res) => {
+  // #swagger.tags = ['Store']
+  // #swagger.description = 'Endpoint para obter lista de lojas.'
+
+  /* #swagger.parameters['limit'] = {
+      description: 'Quantidade de registros a serem retornados',
+      type: 'int'
+  } */
+
+  /* #swagger.parameters['offset'] = {
+      description: 'Quantidade de páginas a serem retornadas',
+      type: 'int'
+  } */
+
   const stores = await Store.findAll({
     limit: parseInt(req.query.limit) || 10,
     offset: parseInt(req.query.offset) || 0
   })
+
+  /* #swagger.responses[200] = { 
+      schema: [{ $ref: "#/definitions/Store" }],
+  } */
+
   res.send(stores)
 }
